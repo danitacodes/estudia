50fd166079c0cd88ab1c7d557229e20dce433a19
Danita Codes <danitacodes@gmail.com>
2022-07-06 22:52:15 -0700

    Create new app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootswatch/dist/lux/bootstrap.min.css";
+import { Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './screens/Home/Home';
+import Signin from './screens/Signin/SigninScreen';
+import Signup from './screens/Signup/Signup';
+import StudyList from './screens/study/Studylist';
+import EditStudy from './screens/Edit/editstudy';
+import StudyPage from './screens/study/Studypage';
+import Profile from './screens/Profile/ProfileScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header />
+     <main>
+     <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/studypage' element={<StudyPage/>} />
+        <Route path='/study/:id' element={<EditStudy/>} />
+        <Route path='/studylist' element={<StudyList/>} />
+     </Routes>
+     </main>
+     <Footer />
+    </>
   );
 }
 
