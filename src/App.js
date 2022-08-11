56f64d0e1415d8a9d55f8d33e951d8446e2ffdb5
Danita Codes <danitacodes@gmail.com>
2022-08-11 13:31:39 -0700

    router error

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import "./App.css";
-import "bootswatch/dist/lux/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './screens/Home/Home';
-import Signin from './screens/Signin/SigninScreen';
-import Signup from './screens/Signup/Signup';
-import StudyList from './screens/study/Studylist';
-import EditStudy from './screens/Edit/editstudy';
-import StudyPage from './screens/study/Studypage';
-import Profile from './screens/Profile/ProfileScreen';
+import "bootswatch/dist/minty/bootstrap.min.css"; // Added this :boom:
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./screens/Home/Home";
+import Signin from "./screens/Signin/SigninScreen";
+import Signup from "./screens/Signup/Signup";
+import StudyList from "./screens/study/Studylist";
+import EditStudy from "./screens/Edit/editstudy";
+import StudyPage from "./screens/study/Studypage";
+import Profile from "./screens/Profile/ProfileScreen";
 
 function App() {
   return (
-    <>
-     <Header />
-     <main>
-     <Router>
-        <Route path='/' component={Home} />
-        <Route path='/signin' component={Signin} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/signup' component={Signup}/>
-        <Route path='/studypage' component={StudyPage} />
-        <Route path='/study/:id' component={EditStudy} />
-        <Route path='/studylist' component={StudyList} />
-     </Router>
-     </main>
-     <Footer />
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/studypage" component={StudyPage} />
+          <Route exact path="/study/:id" component={EditStudy} />
+          <Route exact path="/studylist" component={StudyList} />
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
